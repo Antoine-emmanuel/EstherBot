3978b41c47180bc57c1b8674a3a5af43d43ef3af
Antoine-emmanuel <antoine.sansegundo@gmail.com>
2016-04-22 17:58:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hey, moi c'est poulpy, le poulpe inutile. Dis moi Bloop pour commencer')
                 .then(() => 'speak');
         }
     },
